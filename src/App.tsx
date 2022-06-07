07a35c37089075869adf55b7797ecf5d466ad708
Anastasia <ashmeleva94@gmail.com>
2022-06-07 19:20:49 +0300

    sorting icon fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Container from 'react-bootstrap/Container';
 import EpisodeList from './components/EpisodeList/EpisodeList';
